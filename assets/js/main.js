2d5520a55524e721874003f0ddac27c9278822e3
Md Tofajjal Hossen <59930174+mdtofajjalhossen@users.noreply.github.com>
2021-07-05 01:42:28 +0600

    banner area complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,75 @@
 (function($){
     "use strict";
 
+    function navbarFixed() {
+        var nav_offset_top = $('.header_top_bar').height() + 10;
+        if ($('.fixed').length) {
+            $(window).scroll(function () {
+                var scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $(".fixed").addClass("navbar_fixed");
+                    $('.header_top_bar').css({
+                        "margin-top": "-53px"
+                    });
+                } else {
+                    $(".fixed").removeClass("navbar_fixed");
+                    $('.header_top_bar').css({
+                        "margin-top": "0px"
+                    });
+                }
+            });
+        };
+    };
+    navbarFixed();
+
+
+    function mainSlider() {
+        var BasicSlider = $('.slider_active');
+        BasicSlider.on('init', function (e, slick) {
+            var $firstAnimatingElements = $('.slider_item:first-child').find('[data-animation]');
+            doAnimations($firstAnimatingElements);
+        });
+        BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+            var $animatingElements = $('.slider_item[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+            doAnimations($animatingElements);
+        });
+        BasicSlider.slick({
+            autoplay: true,
+            autoplaySpeed: 5000,
+            dots: true,
+            fade: true,
+            speed: 1000,
+            arrows: false,
+            cssEase: 'linear',
+            // prevArrow:'<button type="button" class="slick-prev"><i class="far fa-chevron-left"></i></button>',
+            // nextArrow:'<button type="button" class="slick-next"><i class="far fa-chevron-right"></i></button>',
+            responsive: [
+                { breakpoint: 850, 
+                    settings: { dots: true, arrows: false } 
+                }
+            ]
+        });
+    
+        function doAnimations(elements) {
+            var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            elements.each(function () {
+                var $this = $(this);
+                var $animationDelay = $this.data('delay');
+                var $animationType = 'animated ' + $this.data('animation');
+                $this.css({
+                    'animation-delay': $animationDelay,
+                    '-webkit-animation-delay': $animationDelay
+                });
+                $this.addClass($animationType).one(animationEndEvents, function () {
+                    $this.removeClass($animationType);
+                });
+            });
+        }
+    }
+    mainSlider();
+
+
+
+
     $('select').niceSelect();
 })(jQuery);
