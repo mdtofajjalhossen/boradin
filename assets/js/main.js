bb09aba8d5d55e4ede780bc7270bcd895f8be31b
Md Tofajjal Hossen <59930174+mdtofajjalhossen@users.noreply.github.com>
2021-07-10 04:01:03 +0600

    about feature service video section create

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,72 @@
 (function($){
     "use strict";
 
+    // function slickFeature(){
+    //     $('.feature_wrap_slide').slick({
+    //         slidesToShow: 3,
+    //         slidesToScroll: 1,
+    //         arrows: true,
+    //         dots: false,
+    //         centerMode: true,
+    //         centerPadding: '0px',
+    //         // cssEase: 'linear',
+    //         pauseOnHover: false,
+    //         // loop: true,
+    //         infinite: true,
+    //         autoplay: false,
+    //         speed: 4000,
+    //         autoplaySpeed: 2000,
+    //         prevArrow:'<button type="button" class="slick-prev"><i class="lnr lnr-chevron-left"></i></button>',
+    //         nextArrow:'<button type="button" class="slick-next"><i class="lnr lnr-chevron-right"></i></button>',
+    //         responsive: [
+    //             {
+    //               breakpoint: 1024,
+    //               settings: {
+    //                 slidesToShow: 3,
+    //                 slidesToScroll: 1,
+    //                 infinite: true,
+    //                 dots: true
+    //               }
+    //             },
+    //             {
+    //               breakpoint: 600,
+    //               settings: {
+    //                 slidesToShow: 2,
+    //                 slidesToScroll: 1
+    //               }
+    //             },
+    //             {
+    //               breakpoint: 480,
+    //               settings: {
+    //                 slidesToShow: 1,
+    //                 slidesToScroll: 1
+    //               }
+    //             }
+    //           ]
+    //       });
+    
+    // }
+    // slickFeature();
+
+
+  /*-------------------------------------
+    WOW
+    -------------------------------------*/
+    // var wow = new WOW({
+    //     boxClass: "wow",
+    //     animateClass: "animated",
+    //     offset: 0,
+    //     mobile: false,
+    //     live: true,
+    //     scrollContainer: null,
+    //   });
+    
+
+
+
+
+
+
     function navbarFixed() {
         var nav_offset_top = $('.header_top_bar').height() + 10;
         if ($('.fixed').length) {
@@ -21,6 +87,8 @@
         };
     };
     navbarFixed();
+
+
 
 
     function mainSlider() {
@@ -70,6 +138,106 @@
 
 
 
+// preloader
+    function Preloaderslide() {
+        $("#preloader").delay(2000).fadeOut("slow");
+    }
+    Preloaderslide();
+
+    // $(window).on('load', function() {
+       
+    //   });
+
+
+
+    //   $(".nav a").on("click", function(){
+    //     $(".nav").find(".active").removeClass("active");
+    //     $(this).parent().addClass("active");
+    //  });
+
 
     $('select').niceSelect();
+
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin: 45,
+        nav:true,
+        items:3,
+        center:false,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        navText: ['<i class="ti-angle-left"></i>', '<i class="ti-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1,
+                margin: 0,
+            },
+            480:{
+                items:1,
+            },
+            767:{
+                items:2,
+                margin: 30,
+            },
+            992:{
+                items:3,
+            },
+            1366:{
+                items:3
+            }
+        }
+    })
+    
+
+ /*-------------------------------------
+    TweenMax Mouse Effect
+    -------------------------------------*/
+    $(".motion-effects-wrap").mousemove(function (e) {
+        parallaxIt(e, ".motion-effects1", -100);
+        parallaxIt(e, ".motion-effects2", -200);
+        parallaxIt(e, ".motion-effects3", 100);
+        parallaxIt(e, ".motion-effects4", 200);
+        parallaxIt(e, ".motion-effects5", -50);
+        parallaxIt(e, ".motion-effects6", 50);
+      });
+    
+      function parallaxIt(e, target_class, movement) {
+        var $wrap = $(e.target).parents(".motion-effects-wrap");
+        if (!$wrap.length) return;
+        var $target = $wrap.find(target_class);
+        var relX = e.pageX - $wrap.offset().left;
+        var relY = e.pageY - $wrap.offset().top;
+    
+        TweenMax.to($target, 1, {
+          x: ((relX - $wrap.width() / 2) / $wrap.width()) * movement,
+          y: ((relY - $wrap.height() / 2) / $wrap.height()) * movement,
+        });
+      }
+
+      function videoPopup() {
+        if ($('.popup-youtube').length) {
+          $('.popup-youtube').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false,
+            mainClass: 'mfp-with-zoom mfp-img-mobile',
+          });
+          $('.popup-youtube').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false
+          });
+        }
+      }
+      videoPopup();
+  
+
+
+
 })(jQuery);
